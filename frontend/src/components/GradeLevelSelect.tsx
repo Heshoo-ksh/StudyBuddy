c917d683e12ee36113223b1061de14a50308efd7
amelhady <amelhady@umich.edu>
2023-10-21 16:43:10 -0400

    Created education level page

---
+++ frontend/src/components/GradeLevelSelect.tsx
@@ -0,0 +1,36 @@
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import '../css/gradeLevelSelect.css';
+import { useState } from "react";
+
+function GradeLevelSelect() {
+    const [nextDisabled, setNextDisabled] = useState(true);
+    const [value, setValue] = useState('');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue((event.target as HTMLInputElement).value);
+        setNextDisabled(false);
+    };
+
+  return (
+    <div className="gradeLevelSelect">
+        <h1>Welcome!</h1>
+        <FormControl>
+            <FormLabel id="radio-buttons-group-label">Please Select Your Education Level:</FormLabel>
+            <RadioGroup
+                aria-labelledby="radio-buttons-group-label"
+                name="radio-buttons-group"
+                onChange={handleChange}
+                className="radioGroup"
+                >
+                <FormControlLabel value="elementary" control={<Radio />} label="Elementary School" />
+                <FormControlLabel value="intermediate" control={<Radio />} label="Middle School" />
+                <FormControlLabel value="high school" control={<Radio />} label="High School" />
+                <FormControlLabel value="college" control={<Radio />} label="College" />
+            </RadioGroup>
+        </FormControl>
+        <Button className="nextButton" variant="contained" disabled={nextDisabled} color="success">Next</Button>
+    </div>
+  );
+}
+
+export default GradeLevelSelect;
