9a3740b4fd01d9d7da0d4571f16d405d61f03769
Hisham Odeh <hishamodeh2001@gmail.com>
2023-10-22 10:16:20 -0400

    link mian page

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -3,9 +3,15 @@ from api.getOverview import overview_routes
 from api.getFlashcards import flashcards_routes
 from api.getMultiChoiceQuiz import multiChoiceQuiz_routes
 
+from flask_cors import CORS
+
+
 app = Flask(__name__)
+CORS(app)
+
 overview_routes(app)
 flashcards_routes(app)
 multiChoiceQuiz_routes(app)
 if __name__ == '__main__':
     app.run(debug=True)
+
